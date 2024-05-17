0bf52e45b28f3123af6c0a8aea9635c52ec538d5
Tamal Krishna <tamalkrishna2026@gmail.com>
2024-05-18 01:10:53 +0600

    Added some animation to the card

--- src/pages/Home/AllProducts/AllProducts.tsx
+++ src/pages/Home/AllProducts/AllProducts.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
-import ShowAllProducts from "./ShowAllProducts";
+import '../../../index.css'
+interface Product {
+  name: string;
+  price: number;
+  img: string;
+}
 
 const AllProducts = () => {
-  const [allProducts, setAllProducts] = useState([]);
-  const [loading, setLoading] = useState([true]);
+  const [allProducts, setAllProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetch("http://localhost:5000/all-products")
@@ -12,24 +17,32 @@ const AllProducts = () => {
         setAllProducts(data);
         setLoading(false);
       });
-  },[]);
+  }, []);
   return (
     <div>
-     {
-        loading ? (
-            <p>
-                Loading...
-            </p>
-        ) : (
-            <ul>
-                {
-                    allProducts.map((product, index)=>(
-                       <ShowAllProducts key={product._id} allProducts={allProducts}></ShowAllProducts>
-                    ))
-                }
-            </ul>
-        )
-     }
+      {loading ? (
+        <>
+          <h1>Please Wait...</h1>
+          <span className="loading loading-bars loading-lg"></span>
+        </>
+      ) : (
+        <div className="grid grid-cols-4 max-w-screen-2xl mx-auto">
+          {allProducts.map((product, index) => (
+            <div key={index} className="card w-80 h-[400px] bg-base-100 shadow-xl">
+              <figure className="all-products-zoom-img">
+                <img className="h-60 " src={product.img} alt="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{product.name}</h2>
+                <p>Price: ${product.price}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary"> Buy Now</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
