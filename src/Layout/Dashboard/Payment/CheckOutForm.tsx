15d0a534c1d86c2a76e4864aed25a7a825d7c475
Tamal Krishna <tamalkrishna2026@gmail.com>
2024-05-27 18:12:44 +0600

    Client secret send to backend

--- src/Layout/Dashboard/Payment/CheckOutForm.tsx
+++ src/Layout/Dashboard/Payment/CheckOutForm.tsx
@@ -1,37 +1,93 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
 const CheckOutForm = () => {
-    const stripe = useStripe()
-    const elements = useElements()
-    
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      // Use stripe and elements for further processing
-    };
+  const [error, setError] = useState<string>("");
+  const [clientSecret, setClientSecret] = useState<string | undefined>(
+    undefined
+  );
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4',
-                },
-              },
-              invalid: {
-                color: '#9e2146',
+  const stripe = useStripe();
+  const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", {
+        price: totalPrice,
+      })
+      .then((res) => {
+        console.log("Response from backend",res.data);
+        if (res.data.clientSecret) {
+          setClientSecret(res.data.clientSecret);
+        } else {
+          setError("Failed to fetch client secret");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching client secret:", error);
+        setError("Failed to fetch client secret");
+      });
+  }, [axiosSecure, totalPrice]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("[error from payment method]", error);
+      setError(error.message);
+    } else {
+      console.log("Payment Method", paymentMethod);
+      setError("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
               },
             },
-          }}
-        />
-        <button type="submit" className="text-red-500" disabled={!stripe}>
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <p className="text-red-500 mt-2">{error}</p>
+      {clientSecret === undefined ? (
+       <span className="loading loading-bars loading-lg"></span>
+      ) : (
+        <button
+          type="submit"
+          className="btn bg-cyan-600 text-white mt-3 hover:bg-cyan-600 "
+          disabled={!stripe || !clientSecret}
+        >
           Pay
         </button>
-      </form>
-    );
+      )}
+    </form>
+  );
 };
 
 export default CheckOutForm;
